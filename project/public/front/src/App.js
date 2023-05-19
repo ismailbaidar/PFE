61805bb3ddc6dace7ad9e02408533e9926e9ed1f
ismailbaidar <ismail.baidar198@gmail.com>
2023-05-19 20:27:15 +0100

    some packages

--- project/public/front/src/App.js
+++ project/public/front/src/App.js
@@ -1,8 +1,12 @@
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faHeart} from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   return (
     <div className="App">
+        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon="fa-regular fa-heart" />
 
     </div>
   );
